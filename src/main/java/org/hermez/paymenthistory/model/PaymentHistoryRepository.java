3afe3e5c139ebe8984b725fd803c365ebbccf25e
tokkaiiii <luv_3_U@naver.com>
2024-08-22 09:49:21 +0900

    feature : 결제 기록 레파지토리/마이바티스 매퍼 추가
    결제 기록 레파지토리/마이바티스 매퍼 추가
    root-context.xml

---
+++ src/main/java/org/hermez/paymenthistory/model/PaymentHistoryRepository.java
@@ -0,0 +1,22 @@
+package org.hermez.paymenthistory.model;
+
+/**
+ * hermez서버로 결제 기록 정보를 입력하고 가져오기 위한 클래스입니다.
+ */
+public interface PaymentHistoryRepository {
+
+  /**
+   * 예약시 결제 금액과 결제일자를 저장합니다.
+   *
+   * @param paymentHistory 결제 기록 정보
+   */
+  void save(PaymentHistory paymentHistory);
+
+  /**
+   * 강의 결제 취소/환불 시 결제 기록에 취소 일자를 추가합니다.
+   *
+   * @param merchantUid hermez서버에서 발급한 주문번호
+   */
+  void updateCancelAt(String merchantUid);
+
+}

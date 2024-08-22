04cc02aeaa624bcb848c2b0f5af4418216745b3e
gitdoolgi <gitdoolgi@gmail.com>
2024-08-22 16:24:43 +0900

    feat: Course class 추가

---
+++ src/main/java/org/hermez/course/model/Course.java
@@ -0,0 +1,45 @@
+package org.hermez.course.model;
+
+/**
+ * 게시판 모델 클래스입니다.
+ * @author 엄진수
+ */
+public class Course {
+    private int courseId;
+    private int instructorId;
+    private Long imageId;
+    private String title;
+    private String createAt;
+    private String updateAt;
+    private String description;
+    private String startDate;
+    private String endDate;
+
+    public Course(){}
+
+    /**
+     * Course 생성자 입니다.
+     *
+     * @param courseId
+     * @param instructorId
+     * @param imageId
+     * @param title
+     * @param createAt
+     * @param updateAt
+     * @param description
+     * @param startDate
+     * @param endDate
+     */
+    public Course(int courseId, int instructorId, Long imageId, String title, String createAt, String updateAt, String description, String startDate, String endDate) {
+        this.courseId = courseId;
+        this.instructorId = instructorId;
+        this.imageId = imageId;
+        this.title = title;
+        this.createAt = createAt;
+        this.updateAt = updateAt;
+        this.description = description;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+}
+

c82dcbed9e8078e2d6b56ef14e349670b54dc713
HS-hyeokjin <youngurwls@gmail.com>
2024-08-22 15:38:40 +0900

    feat : ImageService 예외 처리 추가
    ImageService 예외 추가 및 ImageProcessingException 클래스 생성

---
+++ src/main/java/org/hermez/image/exception/ImageProcessingException.java
@@ -0,0 +1,12 @@
+package org.hermez.image.exception;
+
+public class ImageProcessingException extends RuntimeException {
+
+    public ImageProcessingException(String message) {
+        super(message);
+    }
+
+    public ImageProcessingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}

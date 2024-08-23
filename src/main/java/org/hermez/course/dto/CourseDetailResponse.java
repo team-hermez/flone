88c0ec0cf2d0ccba696b393b1a90093f24c84782
gitdoolgi <gitdoolgi@gmail.com>
2024-08-23 16:03:24 +0900

    feat: CourseDetail parameter 추가
    파일 관련 parameter 추가

--- src/main/java/org/hermez/course/dto/CourseDetailResponse.java
+++ src/main/java/org/hermez/course/dto/CourseDetailResponse.java
@@ -12,36 +12,38 @@ import lombok.ToString;
 @Getter
 @ToString
 public class CourseDetailResponse {
-    private int courseId;            // 강좌 ID
-    private int instructorId;        // 강사 ID
-    private String title;            // 과목명
-    private Double coursePrice;      // 강좌 가격
-    private String description;      // 강좌 내용
-    private String startDate;        // 강좌 시작일
-    private String endDate;          // 강좌 종료일
-    private String instructorName;   // 강사명
-    private String subject;          // 과목 (카테고리)
+    private int courseId;
+    private int instructorId;
+    private String title;
+    private int coursePrice;
+    private String description;
+    private String startDate;
+    private String endDate;
+    private String instructorName;
+    private String subject;
     private String grade;
+    private String courseImage;
 
     private CourseDetailResponse() {}
 
      /**
      * 상세 페이지의 생성자입니다.
      *
-     * @param courseId
-     * @param instructorId
-     * @param title
-     * @param coursePrice
-     * @param description
-     * @param startDate
-     * @param endDate
-     * @param instructorName
-     * @param subject
-     * @param grade
+     * @param courseId          강의 고유 코드
+     * @param instructorId      강사 고유 코드
+     * @param title             강의 제목
+     * @param coursePrice       강의 가격
+     * @param description       강의 설명
+     * @param startDate         강의 시작일
+     * @param endDate           강의 종료일
+     * @param instructorName    강사 이름
+     * @param subject           과목
+     * @param grade             교과등급
+     * @param courseImage       강의 이미지                          
      */
-    private CourseDetailResponse(int courseId,int instructorId, String title, Double coursePrice,
+    private CourseDetailResponse(int courseId,int instructorId, String title, int coursePrice,
                                  String description, String startDate, String endDate,
-                                 String instructorName, String subject, String grade) {
+                                 String instructorName, String subject, String grade, String courseImage) {
         this.courseId = courseId;
         this.instructorId = instructorId;
         this.title = title;
@@ -52,8 +54,8 @@ public class CourseDetailResponse {
         this.instructorName = instructorName;
         this.subject = subject;
         this.grade = grade;
+        this.courseImage = courseImage;
     }
-
     /**
      * 생성자 Builder
      * @param courseId
